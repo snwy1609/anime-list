a3cb95402e97ade5d94caa842e283d6fcf8ff756
Jean Esteban <jean@pop-os.localdomain>
2023-10-03 11:11:30 +0800

    new design

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,4 +2,25 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+
+
+  <div class="h-96 w-44 border-solid bg-gray-200">
+    
+    <div class="grid grid-cols-5 bg-gray-100 border-solid border" >
+      <div class=""> 
+        <h1>Hello</h1>
+        <h2>World</h2>
+        <h2>Jennie</h2>
+      </div>
+      <div>
+        <h1>Hiii</h1>
+      </div>
+      <div>
+        <h2>Jean</h2>
+      </div>
+    </div>
+       
+   
+
+  </div>
 </template>
